389b2118866f39ba479027c6e96733d1812859f3
najish <najish.farooqui@gmail.com>
2018-12-19 12:09:08 +0530

    navigation drawer completed
    problem : recycler view is empty

--- app/src/main/java/com/example/applligent/nagoriengineering/DetailsActivity.java
+++ app/src/main/java/com/example/applligent/nagoriengineering/DetailsActivity.java
@@ -1,13 +1,49 @@
 package com.example.applligent.nagoriengineering;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class DetailsActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                if (menuItem.getItemId() == R.id.nav_home) {
+                    Intent intent = new Intent(getApplicationContext(), ItemListActivity.class);
+                    startActivity(intent);
+
+                } else if (menuItem.getItemId() == R.id.nav_copyright) {
+                    Intent intent = new Intent(getApplicationContext(), Copyright.class);
+                    startActivity(intent);
+
+
+                } else if (menuItem.getItemId() == R.id.nav_about) {
+                    Intent intent = new Intent(getApplicationContext(), About.class);
+                    startActivity(intent);
+
+                } else if (menuItem.getItemId() == R.id.nav_exit) {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+
+
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/applligent/nagoriengineering/ItemListActivity.java
+++ app/src/main/java/com/example/applligent/nagoriengineering/ItemListActivity.java
@@ -1,15 +1,22 @@
 package com.example.applligent.nagoriengineering;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import com.example.applligent.nagoriengineering.databinding.ActivityItemListBinding;
 
 public class ItemListActivity extends AppCompatActivity {
     ActivityItemListBinding binding;
     ItemDao itemDao;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +24,39 @@ public class ItemListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_list);
         itemDao = MyNagoriApplication.getDatabase().itemDao();
         binding = DataBindingUtil.setContentView(this, R.layout.activity_item_list);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                if (menuItem.getItemId() == R.id.nav_home) {
+                    Intent intent = new Intent(getApplicationContext(), ItemListActivity.class);
+                    startActivity(intent);
+
+                } else if (menuItem.getItemId() == R.id.nav_copyright) {
+                    Intent intent = new Intent(getApplicationContext(), Copyright.class);
+                    startActivity(intent);
+
+
+                } else if (menuItem.getItemId() == R.id.nav_about) {
+                    Intent intent = new Intent(getApplicationContext(), About.class);
+                    startActivity(intent);
+
+                } else if (menuItem.getItemId() == R.id.nav_exit) {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+
+
+                return true;
+            }
+        });
 
     }
 }
